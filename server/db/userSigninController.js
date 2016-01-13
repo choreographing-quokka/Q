065bb8a06b763d91645ae8b21c1dd06111ba00ef
henry <hlyford11@gmail.com>
2016-01-12 17:43:58 -0800

    Set up send with FB client side; placeholder url to share for now because it can't share localhost urls

--- server/db/userSigninController.js
+++ server/db/userSigninController.js
@@ -1,20 +1,20 @@
 var mongoose = require('mongoose');
-var User = require('./userModel');
+// var User = require('./userModel');
 
 module.exports = {
 
   addUser : function (req, res, next) {
     console.log('req.body is...', req.body);
-    User.findOne({facebook_id: {req.body.id}}).
-      then(function(result){
-        if(!result) {
-          var newUser = new User({name: req.body.name, id: req.body.id});
-          newUser.save();
-          res.send(201);
-        } else {
-          res.send(200);
-        }
-      })
+    // User.findOne({facebook_id: {req.body.id}}).
+    //   then(function(result){
+    //     if(!result) {
+    //       var newUser = new User({name: req.body.name, id: req.body.id});
+    //       newUser.save();
+    //       res.send(201);
+    //     } else {
+    //       res.send(200);
+    //     }
+    //   })
   }
 
 
